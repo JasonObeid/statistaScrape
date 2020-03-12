aff50e9ab0824f87d9ff9d4633a1c2cd8768cbf4
JasonObeid <jason.obeid98@gmail.com>
2020-03-12 12:52:02 -0400

    Annotation #3

--- xAxis.py
+++ xAxis.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+from shutil import copyfile
 
 #todo 1152 manual annotations left
 
@@ -11,11 +12,17 @@ for filePath in fileList:
     yAxis = df.columns[1]
     if(xAxis == 'Unnamed: 0' or xAxis == ''):
         count += 1
-        print(filePath)
-        print('unrecognized column:')
-        print(df.head)
-        newXAxis = input('Replace with:')
-        df.columns = [newXAxis, df.columns[1]]
-        print(df.head())
-        df.to_csv(index=False, path_or_buf=('2Columns/'+filePath))
+        copyfile('2Columns/' + filePath, 'needsLabel/' + filePath)
+        # print(filePath)
+        # print('unrecognized column:')
+        # print(df.head)
+        # newXAxis = input('Replace with:')
+        # df.columns = [newXAxis, df.columns[1]]
+        # print(df.head())
+        # df.to_csv(index=False, path_or_buf=('2Columns/'+filePath))
+    #else:
+        #text = filePath[0:len(filePath)-3] + 'txt'
+        #copyfile('2Columns/'+filePath, 'temp/data/'+filePath)
+        #copyfile('captions/' + text, 'temp/captions/' + text)
+
 print(count)
